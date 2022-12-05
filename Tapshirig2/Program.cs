ea6b90639663a4fa7d89c228b6179ae50e0cebcf
TuranFG <turanfg@code.edu.az>
2022-12-05 22:51:34 +0400

    Add project files.

---
+++ Tapshirig2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tapshirig2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inter number a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Inter number b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("a divide by b");
+            Console.WriteLine(a / b);
+
+        }
+    }
+}
