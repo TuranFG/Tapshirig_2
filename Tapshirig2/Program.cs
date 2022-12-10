10f58a6dee85d371d5ee3af76cd9ea5927d7516c
TuranFG <turanfg@code.edu.az>
2022-12-10 23:03:28 +0400

    Deyishiklik

--- Tapshirig2/Program.cs
+++ Tapshirig2/Program.cs
@@ -8,8 +8,8 @@
             double a = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Inter number b");
             double b = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("a divide by b");
-            Console.WriteLine(a / b);
+            Console.WriteLine("remain of a divide by b");
+            Console.WriteLine(a % b);
 
         }
     }
